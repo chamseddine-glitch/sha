ff67214d90ef096d6f42db83cf2f4ef3dcd76771
chamseddine-glitch <chamseddinenouah@gmail.com>
2025-10-23 18:02:20 +0100

    Delete data directory

--- data/pickup-locations.ts
+++
@@ -1,9 +0,0 @@
-
-export const pickupLocations = [
-    "مكتب Nord et Ouest Express - الجزائر العاصمة",
-    "مكتب Nord et Ouest Express - وهران",
-    "مكتب Nord et Ouest Express - قسنطينة",
-    "مكتب Yalidine - البليدة",
-    "مكتب Yalidine - سطيف",
-    "مكتب توصيل - عنابة",
-];
